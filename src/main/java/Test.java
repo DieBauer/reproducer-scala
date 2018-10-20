7faf799356dc8ae29bd17619a070bbb3ebd08455
Jens Kat <jens.kat@ing.com>
2018-10-20 13:32:00 +0200

    Simplified java class further

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,14 +1,5 @@
 public class Test {
-  public void myClass(final Properties props) {
-    final MyClass myClass = MyClass.apply(props.a, props.b, props.c, props.d);
-    System.out.println(myClass);
-  }
-
-  static class Properties {
-      public Integer a = 2;
-      public Integer b = 16;
-      public Long c = 10000L;
-      public Integer d = 20000;
-  }
+    final MyClass myClass =
+            MyClass.apply(Integer.valueOf(1), Integer.valueOf(2), Long.valueOf(3), Integer.valueOf(4));
 }
 
